bee7b45d56acec40ace1340e09bb7d801f15eaf8
gregfagan <gregfagan@gmail.com>
2020-07-04 16:54:03 -0500

    animation

--- src/ripple.ts
+++ src/ripple.ts
@@ -1,6 +1,52 @@
 import { Shader } from './Shader';
+import { autorun, observable } from 'mobx';
 
-const { glsl, prop, canvas, float, color } = new Shader();
+const { glsl, canvas, gui, prop, float, color } = new Shader();
+
+const options = observable({
+  animate: true,
+});
+
+gui.add(options, 'animate');
+
+prop({
+  name: 'u_time',
+  type: 'float',
+  value: 0,
+  script: (time) => {
+    let raf: number;
+    let lastTime = 0;
+    let t = 0;
+    const update = (elapsed: number) => {
+      const dt = (elapsed - lastTime) / 1000;
+      t += dt;
+      lastTime = elapsed;
+      time.value = t;
+      raf = requestAnimationFrame(update);
+    };
+    autorun(() => {
+      if (options.animate) {
+        lastTime = performance.now();
+        update(lastTime);
+      } else {
+        cancelAnimationFrame(raf);
+      }
+    });
+  },
+});
+
+prop({
+  name: 'u_mouse',
+  type: 'vec2',
+  value: [canvas.clientWidth / 2, canvas.clientHeight / 2],
+  script: (mouse) => {
+    canvas.addEventListener('mousemove', (e) => {
+      mouse.value[0] = e.clientX * window.devicePixelRatio;
+      mouse.value[1] =
+        (canvas.clientHeight - e.clientY) * window.devicePixelRatio;
+    });
+  },
+});
 
 glsl`
 float sdCircle(vec2 p) {
@@ -8,22 +54,11 @@ float sdCircle(vec2 p) {
 }
 
 void main() {
-  vec2 mouse = coord(${prop({
-    name: 'u_mouse',
-    type: 'vec2',
-    value: [0, 0],
-    script: (mouse) => {
-      canvas.addEventListener('mousemove', (e) => {
-        mouse.value[0] = e.clientX * window.devicePixelRatio;
-        mouse.value[1] =
-          (canvas.clientHeight - e.clientY) * window.devicePixelRatio;
-      });
-    },
-  })});
+  vec2 mouse = coord(u_mouse);
   vec3 baseColor = ${color('base_color')};
   vec3 color = ${color('color', '#ffaaaa')};
   float d = sdCircle(st() - mouse);
-  d = step(d, ${float('radius', 0.1)});
+  d = step(d, ${float('radius', 0.05)} + 0.015 * sin(u_time * 5.));
   gl_FragColor = vec4(mix(baseColor, color, d), 1.);
 }
 `;
