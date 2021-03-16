465e1cae9185f7befc31d1e49349c60e367e7088
gregfagan <gregfagan@gmail.com>
2021-03-16 18:36:37 -0500

    update SystemJS

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-import R from 'ramda';
-import REGL, { Vec2 } from 'regl';
-import { stream, event, log } from './stream';
-import { stopwatch, seconds } from './clock';
-import { glsl, uniform, ugui } from './tools';
-import { quad } from './quad';
-import { sdf } from './sdf';
-import { glCoordinatesFromMouseEvent } from './utils';
-import { GUI } from 'dat.gui';
-import { streamProxy, AutoGUI } from './gui';
+import R from "ramda";
+import REGL, { Vec2 } from "regl";
+import { stream, event, log } from "./stream";
+import { stopwatch, seconds } from "./clock";
+import { glsl, uniform, ugui } from "./tools";
+import { quad } from "./quad";
+import { sdf } from "./sdf";
+import { glCoordinatesFromMouseEvent } from "./utils";
+import { GUI } from "dat.gui";
+import { streamProxy, AutoGUI } from "./gui";
 
 const regl = REGL();
 const { canvas } = regl._gl;
 const store = streamProxy();
 const autoAdd = (gui: GUI, target: Record<string, unknown>) => {
-  (['add', 'addColor'] as Extract<keyof GUI, 'add' | 'addColor'>[]).forEach(
-    (method) => {
+  (["add", "addColor"] as Extract<keyof GUI, "add" | "addColor">[]).forEach(
+    method => {
       const original = gui[method];
       gui[method] = <T>(defaultValue: T, name: string, ...args: any) => {
         target[name] = defaultValue;
         // @ts-expect-error
         return original.call(gui, target, name, ...args);
       };
-    },
+    }
   );
   return gui as AutoGUI;
 };
@@ -31,26 +31,23 @@ const gui = autoAdd(new GUI(), store);
 const hasFocus = stream.merge(
   stream(document.hasFocus()),
   stream
-    .merge(event(window, 'focus'), event(window, 'blur'))
-    .map(R.prop('type'))
-    .map(R.equals('focus')),
+    .merge(event(window, "focus"), event(window, "blur"))
+    .map(R.prop("type"))
+    .map(R.equals("focus"))
 );
-const pause = hasFocus.map(R.not).map(log('pause'));
+const pause = hasFocus.map(R.not).map(log("pause"));
 const clock = stopwatch(pause);
 
 // track mouse
 const mouse = stream.merge(
   stream.of<Vec2>([canvas.width / 2, canvas.height / 2]),
-  event<MouseEvent>(canvas, 'mousemove').map(glCoordinatesFromMouseEvent),
+  event<MouseEvent>(canvas, "mousemove").map(glCoordinatesFromMouseEvent)
 );
 
 // Change color every second
 const color = stream.merge<REGL.Vec3, REGL.Vec3>(
   stream.of([0.1, 0.8, 0.5]),
-  clock
-    .pipe(seconds)
-    .map(log('tick'))
-    .map(() => [Math.random(), Math.random(), Math.random()]),
+  clock.pipe(seconds).map(() => [Math.random(), Math.random(), Math.random()])
 );
 
 const draw = glsl`
@@ -59,17 +56,16 @@ ${sdf}
 void main() {
   vec2 p = st();
   vec2 m = coord(${uniform(mouse)});
-  float d = sdCircle(p - m, ${uniform(gui.add(0.1, 'u_radius', 0.1, 1))});
+  float d = sdCircle(p - m, ${uniform(gui.add(0.1, "u_radius", 0.1, 1))});
   d = step(0., d);
   vec3 color = mix(
     vec3(0),
-    ${uniform(color, 'u_color', 'vec3')},
+    ${uniform(color, "u_color", "vec3")},
     1. - d
   );
   gl_FragColor = vec4(color, 1.);
 }
 `;
-console.log(draw.frag);
 
 const render = regl(draw);
 stream.on(() => render(), clock);
--- src/systemjs-typescript.js
+++ src/systemjs-typescript.js
@@ -1,33 +1,30 @@
-(function (global) {
-  const systemJSPrototype = global.System.constructor.prototype;
-  const transform = systemJSPrototype.transform;
-  systemJSPrototype.transform = function (url, source) {
-    // composition of transform is done based on assuming every format
-    // returns its own System.register. So we don't "compose" transforms
-    // but rather treat transforms "fallbacks" where they can select themselves
-    return Promise.resolve(transform.call(this, url, source)).then(
-      async function (_source) {
-        // if there was translation done, then stop
-        if (source !== _source) return _source;
-        if (/^.*\.ts/.exec(url) == null) return source;
+const tsconfig = fetch("tsconfig.json").then(r => r.json());
+const endsWithFileExtension = /\/?\.[a-zA-Z]{2,}$/;
+const endsWithTS = /^.*\.ts/;
 
-        const config = await fetch('./tsconfig.json').then((r) => r.json());
-        return new Promise((resolve, reject) => {
-          const output = ts.transpileModule(source, config);
-          resolve(output.outputText);
-        });
-      },
-    );
-  };
+function hook(global) {
+  const systemJS = global.System.constructor.prototype;
 
   // Automatically resolve bare specifiers with `.ts` extension
-  const endsWithFileExtension = /\/?\.[a-zA-Z]{2,}$/;
-  const originalResolve = systemJSPrototype.resolve;
-  systemJSPrototype.resolve = function () {
-    // apply original resolve to make sure importmaps are resolved first
-    const url = originalResolve.apply(this, arguments);
-    // append .js file extension if url is missing a file extension
-    // return endsWithFileExtension.test(url) ? url : url + '.js';
-    return endsWithFileExtension.test(url) ? url : url + '.ts';
+  const originalResolve = systemJS.resolve;
+  systemJS.resolve = (...args) => {
+    const url = originalResolve.apply(this, args);
+    return endsWithFileExtension.test(url) ? url : url + ".ts";
   };
-})(typeof self !== 'undefined' ? self : global);
+
+  // Always fetch modules so we can apply transformations
+  systemJS.shouldFetch = () => true;
+
+  // Transform TypeScript when fetching
+  systemJS.fetch = async url => {
+    const source = await fetch(url).then(r => r.text());
+    return new Response(
+      endsWithTS.test(url)
+        ? ts.transpileModule(source, await tsconfig).outputText
+        : source,
+      { headers: { "content-type": "application/javascript" } }
+    );
+  };
+}
+
+hook(typeof self !== "undefined" ? self : global);
